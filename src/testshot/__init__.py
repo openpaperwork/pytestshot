5e30fa115c45a71ba26327c55bf8c301fbbc05d3
Jerome Flesch <jflesch@gmail.com>
2016-09-27 01:24:59 +0200

    Add a function to compare the screenshot and the expected output
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- src/testshot/__init__.py
+++ src/testshot/__init__.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 
+import os
+import threading
+
 import PIL.Image
+import pillowfight
 import gi
 gi.require_version('Gdk', '3.0')
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gdk
-from gi.repository import GdkPixbuf
+from gi.repository import GLib
+from gi.repository import Gtk
 
-import time
+
+TESTS_DATA_DIR = os.getenv(
+    'TESTS_DATA_DIR',
+    os.path.join('tests', 'screenshots')
+)
 
 
 def pixbuf2image(pb):
@@ -24,3 +33,69 @@ def screenshot(gdk_window):
         gdk_window, 0, 0, gdk_window.get_width(), gdk_window.get_height()
     )
     return pixbuf2image(pb)
+
+
+class GtkWait(object):
+    def __init__(self):
+        self.condition = threading.Condition()
+
+    def _wake_up(self):
+        self.condition.acquire()
+        try:
+            self.condition.notify_all()
+        finally:
+            self.condition.release()
+
+    def wait(self):
+        self.condition.acquire()
+        try:
+            while Gtk.events_pending():
+                GLib.idle_add(self._wake_up)
+                self.condition.wait()
+        finally:
+            self.condition.release()
+
+
+def wait():
+    GtkWait().wait()
+
+
+def exit():
+    wait()
+    Gtk.main_quit()
+
+
+def assertWindow(test_inst, test_name, gdk_window, focus_on_text=True):
+    wait()
+    orig_img = screenshot(gdk_window)
+
+    out_img = orig_img
+    if focus_on_text:
+        out_img = out_img.resize(
+            (out_img.size[0] * 4, out_img.size[1] * 4),
+            PIL.Image.ANTIALIAS
+        )
+        out_img = pillowfight.swt(out_img, pillowfight.SWT_OUTPUT_BW_TEXT)
+        out_img = out_img.resize(
+            (int(out_img.size[0] / 4), int(out_img.size[1] / 4)),
+            PIL.Image.ANTIALIAS
+        )
+
+    ref_filename = "{}_ref.png".format(test_name)
+    out_filename = "{}_out.png".format(test_name)
+    orig_filename = "{}_orig.png".format(test_name)
+    diff_filename = "{}_diff.png".format(test_name)
+
+    if not os.path.exists(os.path.join(TESTS_DATA_DIR, ref_filename)):
+        out_img.save(os.path.join(TESTS_DATA_DIR, out_filename))
+        orig_img.save(os.path.join(TESTS_DATA_DIR, orig_filename))
+    assert(os.path.exists(os.path.join(TESTS_DATA_DIR, ref_filename)))
+    ref_img = PIL.Image.open(os.path.join(TESTS_DATA_DIR, ref_filename))
+
+    (has_diff, diff_img) = pillowfight.diff(out_img, ref_img)
+    if has_diff:
+        orig_img.save(os.path.join(TESTS_DATA_DIR, orig_filename))
+        out_img.save(os.path.join(TESTS_DATA_DIR, out_filename))
+        diff_img.save(os.path.join(TESTS_DATA_DIR, diff_filename))
+
+    test_inst.assertFalse(has_diff)
--- tests/basic_test.py
+++ tests/basic_test.py
@@ -5,8 +5,6 @@ import unittest
 import gi
 gi.require_version('Gtk', '3.0')
 
-from gi.repository import Gdk
-from gi.repository import GLib
 from gi.repository import Gtk
 
 import testshot
@@ -20,45 +18,34 @@ class GtkWinInstance(object):
         self.window.show_all()
         self.window.set_size_request(640, 480)
 
-        self.condition = threading.Condition()
-
-        thread = threading.Thread(target=self._gtk_thread)
-        thread.start()
+        self.thread = threading.Thread(target=self._gtk_thread)
+        self.thread.start()
 
         time.sleep(1)
 
     def _gtk_thread(self):
         Gtk.main()
 
-    def _wake_up(self):
-        self.condition.acquire()
-        try:
-            self.condition.notify_all()
-        finally:
-            self.condition.release()
-
-    def wait(self):
-        self.condition.acquire()
-        try:
-            while Gtk.events_pending():
-                GLib.idle_add(self._wake_up)
-                self.condition.wait()
-        finally:
-            self.condition.release()
-
     def stop(self):
-        GLib.idle_add(Gtk.main_quit)
+        testshot.wait()
+        self.window.destroy()
+        testshot.wait()
+        testshot.exit()
+        self.thread.join()
 
 
-class TestBasicMainWin(unittest.TestCase):
+class TestBasicWin(unittest.TestCase):
     def setUp(self):
         self.app = GtkWinInstance()
         self.app.start()
 
     def test_screenshot(self):
-        self.app.wait()
+        testshot.wait()
         pil_img = testshot.screenshot(self.app.window.get_window())
         self.assertNotEqual(pil_img, None)
+
+    def test_ref(self):
+        testshot.assertWindow(self, "test_basic", self.app.window.get_window())
 
     def tearDown(self):
         self.app.stop()
