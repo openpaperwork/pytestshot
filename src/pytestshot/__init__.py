134c8a8ea2350be4e9acb08ddf315589ac1375b2
Jerome Flesch <jflesch@gmail.com>
2016-09-29 15:15:44 +0200

    Revert "Make sure Glib/Gobject/gdk are threads-ready"
    This reverts commit 5660de810e5b3b7ddac5827d2c5953fdf69bb383.

--- src/pytestshot/__init__.py
+++ src/pytestshot/__init__.py
@@ -15,13 +15,10 @@ gi.require_version('Gdk', '3.0')
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gdk
 from gi.repository import GLib
-from gi.repository import GObject
 from gi.repository import Gtk
 
 
 GLib.threads_init()
-GObject.threads_init()
-Gdk.threads_init()
 
 
 TESTS_DATA_DIR = os.getenv(
